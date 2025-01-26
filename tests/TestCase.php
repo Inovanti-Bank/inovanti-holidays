9757ed1a64bae9332f06a2b705c489e92af8f347
Jorge Kania <jorgefkania@gmail.com>
2025-01-26 17:49:42 -0300

    feat: organização dos imports

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,8 +2,8 @@
 
 namespace Tests;
 
-use InovantiBank\Holidays\HolidaysServiceProvider;
 use Orchestra\Testbench\TestCase as OrchestraTestCase;
+use InovantiBank\Holidays\Providers\HolidaysServiceProvider;
 
 class TestCase extends OrchestraTestCase
 {
