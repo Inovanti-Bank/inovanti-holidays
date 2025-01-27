87c19821661a362fffcb97b71738be025b3ab1d6
Jorge Kania <jorge.kania@inovanti.com.br>
2025-01-27 10:46:31 -0300

    feat: corrigir importação da classe TestCase para garantir compatibilidade com o framework de testes

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,8 +2,8 @@
 
 namespace Tests;
 
-use Orchestra\Testbench\TestCase as OrchestraTestCase;
 use InovantiBank\Holidays\Providers\HolidaysServiceProvider;
+use Orchestra\Testbench\TestCase as OrchestraTestCase;
 
 class TestCase extends OrchestraTestCase
 {
